155c63861dce7f0ae92c770db948c4853d507433
zak mari <marioush741@gmail.com>
2024-06-20 04:04:24 +0100

    update

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,2 +1,10 @@
-number1 = int(10)
-number2 = int(5)
+number1 = 10
+number2 = 5
+
+Addition = number1 + number2
+Subtraction = number1 - number2
+Multiplication = number1 * number2
+
+print("Addition of number1 and number2 is", Addition)
+print("Subtraction of number1 and number2 is", Subtraction)
+print("Multiplication of number1 and number2 is", Multiplication)

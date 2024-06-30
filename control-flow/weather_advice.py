4045c25f63aedf54b3f6d90a4355954ae581dfb0
zak mari <marioush741@gmail.com>
2024-06-30 18:50:37 +0100

    ss

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -2,21 +2,21 @@ task = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ")
 time_bound = input("Is it time-bound? (yes/no): ")
 
-match priority:
+match priority.lower():
     case "high":
-        reminder = "Reminder:",task," is a high priority task"
+        reminder = f"Reminder: '{task}' is a high priority task"
     case "medium":
-        reminder = "Reminder: ",task," is a medium priority task"
+        reminder = f"Reminder: '{task}' is a medium priority task"
     case "low":
-        reminder = "Note: ",task," is a low priority task"
+        reminder = f"Note: '{task}' is a low priority task"
     case _:
-        reminder = "Invalid priority level. Please enter high, medium, or low."
+        reminder = f"Invalid priority level. Please enter high, medium, or low."
 
-if time_bound == "yes":
+if time_bound.lower() == "yes":
     reminder += " that requires immediate attention today!"
-elif time_bound == "no":
+elif time_bound.lower() == "no":
     reminder += ". Consider completing it when you have free time."
 else:
     reminder = "Invalid input for time-bound. Please enter yes or no."
 
-print(reminder)
+print(f"\n{reminder}")

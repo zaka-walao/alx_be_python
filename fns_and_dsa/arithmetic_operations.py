d24b45e7f88d02fcb653454916722b0506ad7f23
zak mari <marioush741@gmail.com>
2024-07-07 06:00:25 +0100

    update

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,9 +6,9 @@ def perform_operation(num1:float,num2:float,operation):
     if operation == 'multiply':
         return num1*num2
     if operation == 'divide':
-        if num2 == 0:
-            return "Error: Cannot divide by zero"
-        else:
+        if num2 != 0:
             return num1/num2
+        else:
+            return "Error: Cannot divide by zero"
     else:
         return "Error: Invalid operation"

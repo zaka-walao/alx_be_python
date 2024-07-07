f0a6f44b045ea85242130cf8d5e5f43d5c6c8145
zak mari <marioush741@gmail.com>
2024-07-07 20:26:53 +0100

    update

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -5,7 +5,6 @@ def display_menu():
     print("3. View List")
     print("4. Exit")
 
-
 def main():
     shopping_list = []
     while True:
@@ -13,18 +12,16 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            item_add = input("Enter the item you want to add: ").strip()
-            shopping_list.append(item_add)
-            print(f"{item_add} has been added to the shopping list.")
-
+            item = input("Enter the item to add: ").strip()
+            shopping_list.append(item)
+            print(f"'{item}' has been added to the shopping list.")
         elif choice == '2':
-            item_remove = input("Enter the item you want to remove: ").strip()
-            if item_remove in shopping_list:
-                shopping_list.remove(item_remove)
-                print(f"{item_remove} has been removed from the shopping list.")
+            item = input("Enter the item to remove: ").strip()
+            if item in shopping_list:
+                shopping_list.remove(item)
+                print(f"'{item}' has been removed from the shopping list.")
             else:
-                print(f"{item_remove} is not in the shopping list.")
-
+                print(f"'{item}' is not in the shopping list.")
         elif choice == '3':
             if shopping_list:
                 print("Current Shopping List:")
@@ -32,14 +29,11 @@ def main():
                     print(f"{i}. {item}")
             else:
                 print("The shopping list is empty.")
-
         elif choice == '4':
             print("Goodbye!")
             break
         else:
             print("Invalid choice. Please try again.")
 
-
 if __name__ == "__main__":
     main()
-

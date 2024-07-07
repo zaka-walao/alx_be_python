9d68dd759cb8c96c8319d5a54b6f5fce96e42dc6
zak mari <marioush741@gmail.com>
2024-07-07 19:21:53 +0100

    update

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -9,21 +9,21 @@ def main():
     shopping_list = []
     while True:
         display_menu()
-        choice = int(input("Enter your choice: "))
+        choice = input("Enter your choice: ")
 
-        if choice == 1:
-            item_add = input("enter the item you wanna add: ")
+        if choice == '1':
+            item_add = int(input("enter the item you wanna add: "))
             shopping_list.append(item_add)
 
-        elif choice == 2:
+        elif choice == '2':
             item_remove = input("remove an item: ")
             shopping_list.remove(item_remove)
 
-        elif choice == 3:
+        elif choice == '3':
 
 
            print(shopping_list)
-        elif choice == 4:
+        elif choice == '4':
             print("Goodbye!")
             break
         else:
